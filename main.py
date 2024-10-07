80093add19d63f0b1f580ac10cc11677dc7cfbdd
Carlosgit config --global user.email <Carlos>
2024-10-07 16:53:43 -0400

    added files from tutorial

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+#*******************************************************************************************************************
+#Coffee Machine in OOP
+#Carlos Cartagena
+#10/7/2024
+#The purpose of this program is to take previous project of named "Coffee Machine" and -
+#code it in OOP.
+#*******************************************************************************************************************
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+#making new variables
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
